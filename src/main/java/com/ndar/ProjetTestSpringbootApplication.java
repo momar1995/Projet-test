6b9b8fb886c3144d128824d4da5ff8469b2dff0b
momar1995 <toplayene95@gmail.com>
2022-06-12 23:19:55 +0000

    first commit

---
+++ src/main/java/com/ndar/ProjetTestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ndar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTestSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ndar/entities/Product.java
@@ -0,0 +1,57 @@
+package com.ndar.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @JsonFormat(pattern =  "dd-MM-yyyy")
+    private Date dateCreation;
+
+    private String productType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+}
